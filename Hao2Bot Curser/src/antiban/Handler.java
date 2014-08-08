30a8162345ae602ff34bb1740490b5e3430959c3
Luke Scowen <lmscowen@gmail.com>
2014-08-08 12:59:07 +0100

    Re-structure file system take 2

---
+++ Hao2Bot Curser/src/antiban/Handler.java
@@ -0,0 +1,83 @@
+package antiban;
+
+import java.util.Random;
+
+import org.osbot.rs07.script.Script;
+
+import antiban.Antiban;
+
+public class Handler {
+
+	private Script script;
+	private long nextAntiban;
+	
+	private Antiban antiban;
+	
+	public Handler(Script script)
+	{
+		this.script = script;
+		this.nextAntiban = getUnixTime() + randInt(30,120);
+		
+		antiban = new Antiban(script);
+	}
+	
+	public boolean nextAntiBan()
+	{
+		// Check if an anti ban is due.
+		if(getUnixTime() >= nextAntiban)
+			return true;
+		else
+			return false;
+	}
+	
+	public void performAntiBan() throws InterruptedException
+	{
+		// Generate a random number, depending on the number of anti ban functions we have. Find its function.
+		switch(randInt(1,6)){
+			case 1:
+				// Move the camera out of the screen and sleep.
+				antiban.simulateAltTab();
+			break;
+			case 2:
+				// Open friends tab and sleep.
+				antiban.checkFriendsOnline();
+			break;
+			case 3:
+				// Move camera pitch/yaw.
+				antiban.moveCamera();
+			break;
+			case 4:
+				// Open inventory and sleep.
+				antiban.checkInventory();
+			break;
+			case 5:
+				// Open skills tab and hover over magic.
+				antiban.checkSkill();
+			break;
+			case 6:
+				// Use pre-defined methods and move mouse randomly.
+				antiban.randomMouseMovement();
+			break;
+			default:
+				
+			break;
+		}
+		// Add some time to the next anti ban
+		nextAntiban = getUnixTime() + randInt(30,90);
+	}
+	
+	// Using unix time to determine when to next perform major and minor anti bans
+	private long getUnixTime()
+	{
+		return System.currentTimeMillis() / 1000L;
+	}
+	
+	// Generate a random in range integer
+	public int randInt(int min, int max) {
+	    Random rand = new Random();
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+	
+}
---
+++ Hao2Bot Curser/src/main/State.java
@@ -0,0 +1,7 @@
+package main;
+
+public enum State {
+	CAST,
+	FIND_TARGET,
+	MOVE;
+}

0137351049c10b313ca1aa2f9291e8922a8e2ae6
Scowen <lmscowen@gmail.com>
2014-08-09 13:41:34 +0100

    GUI Integration and paint updates

--- Hao2Bot Curser/src/main/Main.java
+++ Hao2Bot Curser/src/main/Main.java
@@ -14,9 +14,12 @@ import action.Casting;
 import action.Move;
 import action.Hunt;
 import antiban.Handler;
+import gui.GUI;
 
+import org.osbot.rs07.api.Skills;
 import org.osbot.rs07.api.map.Position;
 import org.osbot.rs07.api.model.NPC;
+import org.osbot.rs07.api.ui.Skill;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
@@ -31,23 +34,39 @@ public class Main extends Script
 {
 	
 	private Script script;
-	private long scriptStartTime;
-	private String proposedTarget;
+	private long startScriptTime;
+	private int startMagicExp;
+	private int startMagicLevel;
+	private String phMagicExp;
+	
+	public static String proposedTarget;
 	private NPC target;
 	private Position savedPosition;
 	private String status;
-	Image paint = getImage("http://i60.tinypic.com/2a50hg2.png");
+	Image paint = getImage("http://www.ourgoose.com/paint.png");
+	public static boolean guiActive;
 	
 	private Casting casting;
 	private Move move;
 	private Hunt hunt;
 	private Handler antiBanHandler;
+	private GUI gui;
+	
 	
 	@Override
 	public void onPaint(Graphics2D g)
-	{		
-		// g.setColor(Color.BLACK);
-		g.drawImage(this.paint, 251, 344, null);
+	{	
+		// Use the X and Y for paint relative items.
+		int x = 251, y = 344;
+		
+		// Draw the paint image.
+		g.drawImage(this.paint, x, y, null);
+		
+		// Draw the information.
+		g.setColor(Color.WHITE);
+		g.drawString("Status: " + status, x + 10, y + 40);
+		g.drawString("Run Time: " + runTime(startScriptTime), x + 10, y + 60);
+		g.drawString("Exp (p/h): " + (script.skills.getExperience(Skill.MAGIC) - startMagicExp) + "(" + phMagicExp  + "k)", x + 10, y + 90);
 	}
 	
 	@Override
@@ -56,7 +75,10 @@ public class Main extends Script
 		status = "Initializing";
 			
 		// Set starting script time.
-		scriptStartTime = System.currentTimeMillis();
+		startScriptTime = System.currentTimeMillis();
+		// Set starting magic experience and level
+		startMagicExp = script.skills.getExperience(Skill.MAGIC);
+		startMagicLevel = script.skills.getStatic(Skill.MAGIC);
 		
 		// Initialise classes.
 		casting = new Casting(this);
@@ -64,25 +86,26 @@ public class Main extends Script
 		hunt = new Hunt(this);
 		antiBanHandler = new Handler(this);
 		
+		proposedTarget = null;
+		
 		// Set some nulls yo!
 		savedPosition = null;
 		target = null;
-		// TODO: TEMPORARY
-		proposedTarget = "Monk of Zamorak";
+		gui = new GUI();
+		guiActive = true;
+		gui.setVisible(true);
 	}
 	
 	@Override
 	public int onLoop() throws InterruptedException 
-	{
-		// If there isn't a saved position, set it.
-		if(savedPosition == null){
-			// Set the saved position to where the user is currently located.
-			savedPosition = new Position(
-				myPosition().getX(),
-				myPosition().getY(),
-				myPosition().getZ()
-			);
-		}
+	{		
+		
+		
+		if(checkGUIActive())
+			return 100;
+		
+		// Check the script has placed a saved position
+		checkSavedPosition();
 		
 		// Get the state of the environment.
 		State state = getState();
@@ -116,7 +139,10 @@ public class Main extends Script
 			antiBanHandler.performAntiBan();
 		}
 		
-		return 50;
+		// Get the magic exp per hour
+		getExpPerHour();
+		
+		return 60;
 	}
 	
 	private State getState()
@@ -141,6 +167,40 @@ public class Main extends Script
 		return State.CAST;
 	}
 	
+	public void getExpPerHour()
+	{
+		phMagicExp = Integer.toString(Math.round((expPerHour(startScriptTime, startMagicExp, Skill.MAGIC) / 1000)), 1);
+	}
+	
+	public boolean checkGUIActive()
+	{
+		// If the GUI is still open, wait for that.
+		if(guiActive){
+			// Check if the GUI has been closed.
+			if(!gui.isActive() || !gui.isVisible() || proposedTarget != null){
+				guiActive = false;
+				return false;
+			}
+		}
+		
+		status = "Waiting for target selection";
+		
+		return true;
+	}
+	
+	public void checkSavedPosition()
+	{
+		// If there isn't a saved position, set it.
+		if(savedPosition == null){
+			// Set the saved position to where the user is currently located.
+			savedPosition = new Position(
+				myPosition().getX(),
+				myPosition().getY(),
+				myPosition().getZ()
+			);
+		}
+	}
+	
 	public void logout(String message)
 	{
 		// Log out the user, display the message given in the console and stop the script.
@@ -158,6 +218,10 @@ public class Main extends Script
 		millis -= minutes * 60000L;
 		long seconds = millis / 1000L;
 		return nf.format(hours) + ":" + nf.format(minutes) + ":" + nf.format(seconds);
+	}
+	
+	public int expPerHour(double startTime, int startExp, Skill skill){
+		return ((int)(3600000.0D / (System.currentTimeMillis() - startTime) * (script.skills.getExperience(skill) - startExp)));
 	}
 
 	private Image getImage(String url)
